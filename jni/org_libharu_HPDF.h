e9a4329a51b15345c8e16b41b7e5299142b9cf86
vbraun <vbraun@localhost>
2012-02-13 18:22:51 -0800

    added another JNI header

---
+++ jni/org_libharu_HPDF.h
@@ -0,0 +1,27 @@
+#ifndef org_libharu_HPDF__H
+#define org_libharu_HPDF__H
+
+#include <jni.h>
+#include "hpdf.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// access to the HPDF_Page field in org.libharu.Page
+HPDF_Page get_HPDF_Page(JNIEnv *env, jobject obj);
+void set_HPDF_Page(JNIEnv *env, jobject obj, HPDF_Page ptr);
+
+// access to the HPDF_Doc field in org.libharu.Document
+HPDF_Doc get_HPDF_Doc(JNIEnv *env, jobject obj);
+void set_HPDF_Doc(JNIEnv *env, jobject obj, HPDF_Doc ptr);
+
+// access to the HPDF_Font field in org.libharu.Font
+HPDF_Font get_HPDF_Font(JNIEnv *env, jobject obj);
+void set_HPDF_Font(JNIEnv *env, jobject obj, HPDF_Font ptr);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
