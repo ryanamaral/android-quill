151def7f33a39e7e95d89fa01b39de0004ddb590
vbraun <vbraun@localhost>
2012-03-19 15:54:15 -0400

    Galaxy Note fixes

--- src/name/vbraun/lib/pen/PenEventICS.java
+++ src/name/vbraun/lib/pen/PenEventICS.java
@@ -6,9 +6,10 @@ public class PenEventICS extends PenEvent {
 	// The device reports pen events like Ice Cream Sandwich
 	// E.g. HTC Honeycomb devices
 	
+	final static int SOURCE_STYLUS = 0x00004002;
+
 	@Override
 	public boolean isPenEvent(MotionEvent event) {
-		int SOURCE_STYLUS = 0x00004002;
 		// == InputDevice.SOURCE_STYLUS  in ICS
 		return (event.getSource() & SOURCE_STYLUS) == SOURCE_STYLUS;
 	}
---
+++ src/name/vbraun/lib/pen/PenEventSamsungNote.java
@@ -0,0 +1,21 @@
+package name.vbraun.lib.pen;
+
+import android.util.Log;
+import android.view.InputDevice;
+import android.view.MotionEvent;
+
+public class PenEventSamsungNote extends PenEvent {
+	private static final String TAG = "PenEventSamsungNote";
+	
+	/**
+	 * Hello Samsung, why are you not setting SOURCE_STYLUS on your pen?
+	 */
+	final static int SOURCE_S_PEN = InputDevice.SOURCE_KEYBOARD | InputDevice.SOURCE_CLASS_POINTER; 
+	
+	public boolean isPenEvent(MotionEvent event) {
+		InputDevice dev = event.getDevice();
+		Log.v(TAG, "Touch: "+dev.getId()+" "+dev.getName()+" "+event.getSource()+" "+dev.getSources()+" ");
+		return (event.getDevice().getSources() & SOURCE_S_PEN) == SOURCE_S_PEN;
+	}
+	
+}
