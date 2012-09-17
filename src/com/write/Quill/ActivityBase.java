5349d8eda836f8ab29067a0fef37ffaeeaae0d17
vbraun <vbraun@localhost>
2012-09-17 15:16:55 +0100

    add auto-save every 15 mins

--- src/com/write/Quill/ActivityBase.java
+++ src/com/write/Quill/ActivityBase.java
@@ -3,6 +3,7 @@ package com.write.Quill;
 import name.vbraun.lib.pen.Hardware;
 import name.vbraun.view.write.HandwriterView;
 
+import com.write.Quill.data.Book;
 import com.write.Quill.data.Bookshelf;
 import com.write.Quill.data.StorageAndroid;
 
@@ -17,8 +18,13 @@ import android.widget.Toast;
 
 /**
  * Base class for all Quill activities
- * It keeps track of how many activities are running, and will backup notebooks when 
- * no more activities are visible (i.e. the user has navigated away)
+ * 
+ *  - Reference counter keeps track of how many activities are running
+ *  - automatic backup notebooks when no more activities are visible 
+ *    (i.e. the user has navigated away)
+ *  - periodically save the current notebook
+ *  - Removal of deprecated preferences
+ * 
  * @author vbraun
  *
  */
@@ -29,6 +35,7 @@ public class ActivityBase extends Activity {
 	private static Context context;
 	private static final Handler backupHandler = new Handler();
 	private static boolean firstRun = true;
+	private static long lastUserInteraction = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,15 +48,25 @@ public class ActivityBase extends Activity {
       	}
 	}
 	
+	/* Keep track of the last user interaction so we do not save while he is doing something
+	 */
+	@Override
+	public void onUserInteraction() {
+		lastUserInteraction = System.currentTimeMillis();
+		super.onUserInteraction();
+	}
+	
 	@Override
 	protected void onResume() {
 		quillIncRefcount();
+		backupHandler.postDelayed(automaticSave, automaticSaveDelay);
 		super.onResume();
 	}
 	
 	@Override
 	protected void onPause() {
 		super.onPause();
+		backupHandler.removeCallbacks(automaticSave);
 		quillDecRefcount();
 	}
 	
@@ -79,6 +96,22 @@ public class ActivityBase extends Activity {
     	}
     };
     
+    private final static int automaticSaveDelay = 15 * 60 * 1000;
+    private final static int automaticSaveIdle = backupDelay;
+    
+    private static Runnable automaticSave = new Runnable() {
+    	public void run() {
+    		long idle = System.currentTimeMillis() - lastUserInteraction;
+    		if (idle < automaticSaveIdle) {
+    			backupHandler.postDelayed(automaticSave, automaticSaveIdle/3*2);
+    		} else {
+    			Book book = Bookshelf.getCurrentBook();
+    			book.save();
+    			Toast.makeText(context, R.string.activity_base_automatic_saved, Toast.LENGTH_SHORT).show();
+    			backupHandler.postDelayed(automaticSave, automaticSaveDelay);
+    		}
+    	}
+    };
     
 	private static final String KEY_DEBUG_OPTIONS = HandwriterView.KEY_DEBUG_OPTIONS;
 	private static final String KEY_HIDE_SYSTEM_BAR = Preferences.KEY_HIDE_SYSTEM_BAR;
