ada22f5d36bc01e9602a298f347e1f0ac7c5d43e
vbraun <vbraun@localhost>
2012-11-12 22:39:45 -0500

    Import by file extension works now

--- src/com/write/Quill/bookshelf/ImportBackupActivity.java
+++ src/com/write/Quill/bookshelf/ImportBackupActivity.java
@@ -5,13 +5,18 @@ import java.io.File;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.write.Quill.ActivityBase;
+import com.write.Quill.QuillWriterActivity;
 import com.write.Quill.R;
+import com.write.Quill.data.Book.BookIOException;
+import com.write.Quill.data.Bookshelf;
 
 /**
- * This class is called if the user picks a .quill file with a file manager
+ * This activity is called if the user picks a .quill file with a file manager
  * @author vbraun
  *
  */
@@ -25,6 +30,7 @@ public class ImportBackupActivity extends ActivityBase {
 		setContentView(R.layout.import_backup);
 	}
 	
+	private Handler handler = new Handler();
 	private File file = null;
 	
 	@Override
@@ -36,12 +42,41 @@ public class ImportBackupActivity extends ActivityBase {
 	    if (uri == null) return;
         final String filename = uri.getEncodedPath();
         file = new File(filename);
+        Log.e(TAG, "importing backup file "+file.getAbsolutePath());
 	}
 	
+	private Runnable importRunnable = new Runnable() {
+		@Override
+		public void run() {
+    		Bookshelf bookshelf = Bookshelf.getBookshelf();
+    		try {
+    			bookshelf.importBook(file);
+    		} catch (BookIOException e) {
+    			Log.e(TAG, "Error loading the backup file.");
+    			Toast.makeText(ImportBackupActivity.this, 
+    					R.string.preferences_err_loading_backup, Toast.LENGTH_LONG).show();
+    			return;
+    		}
+    		startQuill();
+		}
+	};
+	
+	/**
+	 * Start the mail Quill activity
+	 * used after import is finished
+	 */
+	private void startQuill() {
+		Intent intent = new Intent(this, QuillWriterActivity.class);
+		startActivity(intent);
+		finish();
+	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
-		Log.e(TAG, "onResume");
+		if (file == null) 
+			finish();
+		else
+			handler.postDelayed(importRunnable, 1000);
 	}
 }
